d552effeeb0c561a1d3ce22b68536b203f6e9689
tvthanh <tvthanh.cod@gmail.com>
2025-05-27 16:14:07 +0700

    update setup ota

--- App.tsx
+++ App.tsx
@@ -1,7 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Updates from 'expo-updates';
+import { useEffect } from 'react';
+
+
+
+const checkUpdate = async () => {
+  const update = await Updates.checkForUpdateAsync();
+  if (update.isAvailable) {
+    await Updates.fetchUpdateAsync();
+    await Updates.reloadAsync(); // Khởi động lại app để dùng bundle mới
+  }
+};
+
 
 export default function App() {
+
+  useEffect(() => {
+    checkUpdate();
+  }, [])
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
