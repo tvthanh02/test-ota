31b30956eaf78b20e9be7e11ac377912ceb475c5
tvthanh <tvthanh.cod@gmail.com>
2025-06-11 15:33:41 +0700

    update

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+
+
+const App = () => {
+    return (
+        <Text>Home</Text>
+    )
+}
+
+export default App
---
+++ app/settings.tsx
@@ -0,0 +1,9 @@
+import { Text } from "react-native"
+
+
+
+const Setting = () => {
+    return <Text>Settings</Text>
+}
+
+export default Setting
